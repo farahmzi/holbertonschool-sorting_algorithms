59d84a5ce70b682b0c2c5390157fd1aa075cf934
farahmzi <farahmzi@gmail.com>
2025-06-21 20:29:55 +0000

    Fix Betty style and complete selection sort implementation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,29 +1,30 @@
 #include "sort.h"
+
 /**
-*selection_sort-Sorts an array using selection sort algorithm
-*@array:Array to be sorted
-*@size:Size of the array
-*/
-void selection_sort(int *array,size_t size)
+ * selection_sort - Sorts an array of integers using selection sort
+ * @array: The array to sort
+ * @size: Size of the array
+ */
+void selection_sort(int *array, size_t size)
 {
-size_t i,j,min;
+size_t i, j, min_idx;
 int temp;
-if(array==NULL||size<2)
+if (array == NULL || size < 2)
 return;
-for(i=0;i<size-1;i++)
+for (i = 0; i < size - 1; i++)
 {
-min=i;
-for(j=i+1;j<size;j++)
+min_idx = i;
+for (j = i + 1; j < size; j++)
 {
-if(array[j]<array[min])
-min=j;
+if (array[j] < array[min_idx])
+min_idx = j;
 }
-if(min!=i)
+if (min_idx != i)
 {
-temp=array[i];
-array[i]=array[min];
-array[min]=temp;
-print_array(array,size);
+temp = array[i];
+array[i] = array[min_idx];
+array[min_idx] = temp;
+print_array(array, size);
 }
 }
 }
