ab7a7dbb83ecf6348f022110e4bd2a86f6abac5d
farahmzi <farahmzi@gmail.com>
2025-06-21 19:04:08 +0000

    Add bubble sort implementation and sort.h header

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,27 @@
 #include "sort.h"
 /**
-*bubble_sort-Sorts an array of integers using Bubble Sort
-*@array:The array to sort
+*bubble_sort-Sorts an array of integers in ascending order
+*using the Bubble sort algorithm
+*@array:Array to be sorted
 *@size:Size of the array
-*Description:Prints the array after each swap
 */
 void bubble_sort(int *array, size_t size)
 {
-size_t i,j;
-int tmp,swapped;
-if(!array||size<2)
+size_t i, j;
+int temp;
+if (array == NULL || size < 2)
 return;
-for(i=0;i<size-1;i++)
+for (i = 0; i < size - 1; i++)
 {
-swapped=0;
-for(j=0;j<size-i-1;j++)
+for (j = 0; j < size - 1 - i; j++)
 {
-if(array[j]>array[j+1])
+if (array[j] > array[j + 1])
 {
-tmp=array[j];
-array[j]=array[j+1];
-array[j+1]=tmp;
-print_array(array,size);
-swapped=1;
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
 }
 }
-if(!swapped)
-break;
 }
 }
